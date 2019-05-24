9f81db9453012f44d782af619d3ca76a53a226ac
Jason Seminara <jseminara@gmail.com>
2019-05-23 23:22:27 -0400

    updates DataDropDown

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-interface JSONshape {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
-
-
-const useFetch = (url:URL['href']):JSONshape|null => {
-  const [data, setData] = useState(null);
-
-  async function fetchData() {
-    const response:Response = await fetch(url);
-    const json = await response.json();
-    setData(json);
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, [url]);
-
-  return data;
-};
-
-function Example() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-       Click me
-      </button>
-    </div>
-  );
-}
+import React from 'react';
+import DataDropDown from './DataDropDown';
 
 const App: React.FC = () => {
-  return <Example />
+  return <DataDropDown />
 
 };
 
---
+++ src/DataDropDown/index.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { MenuItem } from "@blueprintjs/core";
+import { Suggest, ItemRenderer } from '@blueprintjs/select';
+import '@blueprintjs/core/lib/css/blueprint.css';
+
+const useFetch = (url:URL['href']):[string[], (word:string) => void] => {
+  const [data, setData] = useState([]);
+
+  async function fetchData(word:string) {
+    try {
+      const response:Response = await fetch(`${url}?search=${word}`);
+      const responseData = await response.json();
+      setData(responseData.slice(0,10));
+    } catch (e) {
+      debugger;
+    }
+  }
+
+  return [data, fetchData];
+};
+
+const renderWord:ItemRenderer<string> = (item, { handleClick, modifiers, query }) => {
+  console.log(item, query)
+  return (
+    <MenuItem
+      active={modifiers.active}
+      disabled={modifiers.disabled}
+      onClick={handleClick}
+      text={item}
+      key={item}
+    />
+    )
+}
+
+
+const DataDropDown:React.FC = () => {
+  const [symbols, setSymbols] = useFetch('/words');
+
+
+  return (
+    <section className="bp3-dark">
+      <Suggest
+        items={symbols as string[]}
+        inputValueRenderer={item => item}
+        onQueryChange={setSymbols}
+        itemRenderer={renderWord}
+        openOnKeyDown
+        popoverProps={{ minimal:true }}
+       />
+    </section>
+    )
+}
+
+export default DataDropDown;
